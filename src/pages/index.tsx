65b6b662553b7e234cb97e05b0c7849a2cdaa3ef
Andrew <duyquang.fit@gmail.com>
2025-02-27 23:22:51 +0700

    fix: update docs baseurl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import type { ReactNode } from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/docs/" />;
+  return <Redirect to="/optimai/" />;
 }
