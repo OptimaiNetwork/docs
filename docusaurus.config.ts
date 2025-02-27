e3544d6158413780ced5bea2507ca93a5350e1f4
Andrew <duyquang.fit@gmail.com>
2025-02-27 20:10:21 +0700

    fix: update build config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   organizationName: 'OptimaiNetwork', // Usually your GitHub org/user name.
   projectName: 'OptimAI Network Docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
