23552ef135fae0b0b3629278f25203c402803581
anonsyn <tdn352001@gmail.com>
2025-03-11 13:19:08 +0700

    feat: enhance table of contents styling and layout

---
+++ src/theme/DocRoot/Layout/Main/index.tsx
@@ -0,0 +1,17 @@
+import { useDocsSidebar } from '@docusaurus/plugin-content-docs/client'
+import type { Props } from '@theme/DocRoot/Layout/Main'
+import clsx from 'clsx'
+import { type ReactNode } from 'react'
+
+import styles from './styles.module.scss'
+
+export default function DocRootLayoutMain({ hiddenSidebarContainer, children }: Props): ReactNode {
+  const sidebar = useDocsSidebar()
+  return (
+    <main
+      className={clsx(styles.docMainContainer, (hiddenSidebarContainer || !sidebar) && styles.docMainContainerEnhanced)}
+    >
+      <div className={clsx(styles.docItemWrapper)}>{children}</div>
+    </main>
+  )
+}
