5f9e767caf776fd784d68593b8e36fff0774ce59
anonsyn <tdn352001@gmail.com>
2025-03-12 14:26:34 +0700

    feat: implement admonition components with custom styling and icons

---
+++ src/theme/PaginatorNavLink/index.tsx
@@ -0,0 +1,87 @@
+import Link from '@docusaurus/Link'
+import type { Props } from '@theme/PaginatorNavLink'
+import clsx from 'clsx'
+import { type ReactNode } from 'react'
+
+export default function PaginatorNavLink(props: Props): ReactNode {
+  const { permalink, title, subLabel, isNext } = props
+  return (
+    <Link
+      className={clsx('pagination-nav__link', isNext ? 'pagination-nav__link--next' : 'pagination-nav__link--prev')}
+      to={permalink}
+    >
+      {subLabel && (
+        <div className="pagination-nav__sublabel">
+          {!isNext && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m11 17-5-5 5-5" />
+              <path d="m18 17-5-5 5-5" />
+            </svg>
+          )}
+          <span>{subLabel}</span>
+          {isNext && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m6 17 5-5-5-5" />
+              <path d="m13 17 5-5-5-5" />
+            </svg>
+          )}
+        </div>
+      )}
+      <div className="pagination-nav__label">
+        {!isNext && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m11 17-5-5 5-5" />
+            <path d="m18 17-5-5 5-5" />
+          </svg>
+        )}
+        <span>{title}</span>
+        {isNext && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m6 17 5-5-5-5" />
+            <path d="m13 17 5-5-5-5" />
+          </svg>
+        )}
+      </div>
+    </Link>
+  )
+}
